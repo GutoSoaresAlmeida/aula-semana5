5d75c9d15ebb631e4c57f9dcd8e6e5959514f5bc
GutoSoaresAlmeida <augusto.almeida@yduqs.com.br>
2025-03-07 17:52:33 -0300

    Entrega Semana 6 Final

--- src/Filtro/httpExceptionFilter.ts
+++ src/Filtro/httpExceptionFilter.ts
@@ -5,13 +5,14 @@ import {
     HttpException,
     InternalServerErrorException,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Response, Request } from 'express'; // Importando Request
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: unknown, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>(); // Obtendo o request
 
         let status = 500;
         let message = 'Erro interno do servidor';
@@ -29,10 +30,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
             exception = new InternalServerErrorException();
         }
 
+       
         response.status(status).json({
             statusCode: status,
             message,
             timestamp: new Date().toISOString(),
+            path: request.url, // Caminho da requisição
+            method: request.method, // Método HTTP da requisição
         });
     }
 }
